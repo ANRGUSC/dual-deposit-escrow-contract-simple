0e26b4010f6d78ea4eaebfb916640beea447f1ab
Bhaskar Krishnamachari <bkrishna@usc.edu>
2023-03-21 12:46:54 -0700

    Update DoubleDepositEscrow.sol

--- DoubleDepositEscrow.sol
+++ DoubleDepositEscrow.sol
@@ -22,16 +22,18 @@ contract DoubleDepositEscrow {
     
     function buyer_deposit() payable public {
         require(msg.sender == buyer, "Only buyer can deposit.");
+        require(!buyerDeposited, "Buyer has already deposited.");
         require(msg.value == depositAmount, "Deposit amount should be equal to the deposit amount specified.");
         buyerDeposited = true;      
     }
     
     function seller_deposit() payable public {
         require(msg.sender == seller, "Only seller can deposit.");
+        require(!sellerDeposited, "Seller has already deposited.");
         require(msg.value == depositAmount, "Deposit amount should be equal to the deposit amount specified.");
         sellerDeposited = true;
     }
-    
+ 
     function approve() public {
         require(msg.sender == buyer, "Only buyer can approve.");
         require(!isApproved, "Transaction has already been approved.");
